6a87f3b4fc41eea7f9a20196e661c60a8eee9c46
SarmisthaMohanty <sarmmohanty@deloitte.com>
2024-04-22 12:56:26 +0530

    CICD trigger

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+	languageOptions:{
+		ecmaVersion: 2022,
+		sourceType: "module"
+	},
+   rules: {
+      "no-tabs": "warn",
+      "camelcase": "warn",
+      "indent": "warn",
+      "no-var": "warn",
+      "quotes": "warn",
+      "no-trailing-spaces": "warn",
+      "no-mixed-spaces-and-tabs": "warn",
+      "comma-dangle": "warn",
+      "spaced-comment": "warn",
+      "keyword-spacing": "warn",
+      "eol-last": "warn",
+      "space-before-blocks": "warn",
+      "brace-style": "warn",
+      "max-len": "warn",
+      "padded-blocks": "warn",
+      "linebreak-style": "warn"
+   }
+}
